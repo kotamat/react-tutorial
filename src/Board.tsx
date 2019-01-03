97ff943a2e124cc94a772f009b16e6e442182667
kotamat <kota1861@gmail.com>
2019-01-04 01:27:44 +0900

    add calculator

--- src/Board.tsx
+++ src/Board.tsx
@@ -24,11 +24,21 @@ export class Board extends React.Component<Props, State> {
   }
   handleClick(i: number) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
   render() {
-    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    const winner = calculateWinner(this.state.squares);
+    let status: string;
+    console.log(winner);
+    if (winner) {
+      status = `Winner ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -50,6 +60,26 @@ export class Board extends React.Component<Props, State> {
       </div>
     );
   }
+}
+function calculateWinner(squares: State["squares"]): string | null {
+  const lines: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (const line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
